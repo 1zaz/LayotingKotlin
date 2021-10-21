d0a433244e15f8d02f73b53bc27ef0c771072d60
AzisMM <mabdulaziz124@gmail.com>
2021-10-21 16:35:51 +0700

    Add Layout Simple

---
+++ app/src/main/java/com/azis/layotingkotlin/SimpleLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.azis.layotingkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SimpleLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simple_layout)
+    }
+}
